a7df58eea6735d4b60f794f0f3f9df2d353c39b5
Austin Vigo <avigo@uci.edu>
2020-03-08 10:35:23 -0700

    Initialized the react-app
    Initialized react app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//Route setup
+app.get('/', (req, res) => {    
+    res.send('root route');
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Start server
+app.listen(port, (req, res) => {
+    console.log(`server listening on port: ${port}`)
+ });
